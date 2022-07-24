05f616e8ea5f5af0c77716310acd216353d201b1
h-wi <108285793+h-wi@users.noreply.github.com>
2022-07-25 03:31:48 +0900

    Add files via upload

---
+++ .파이썬 기초 문제/list_Q2.py
@@ -0,0 +1,5 @@
+a=['Life','is','too','short']
+result=" ".join(a) 
+print(result)
+
+#join 함수를 통해 리스트를 문자열로 이동할 수 있다.
---
+++ .파이썬 기초 문제/set.py
@@ -0,0 +1,7 @@
+a=[1,1,1,2,2,3,3,3,4,4,5]
+aSet=set(a)
+b=list(aSet)
+print(b)
+
+#set 자료형은 중복 요소가 없다.
+#자료형을 변환 할땐 list(),tuple(),set() !!
