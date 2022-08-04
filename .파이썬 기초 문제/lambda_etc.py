ab6cd6ee04c70ec0538cddf5619f13c83b8a4ec4
h-wi <108285793+h-wi@users.noreply.github.com>
2022-08-04 14:32:10 +0900

    Create lambda_etc.py

---
+++ .파이썬 기초 문제/lambda_etc.py
@@ -0,0 +1,9 @@
+print(list(filter(lambda x : x > 0, [1,-2,3,-5,8,3])))
+
+print(list(map((lambda x : x*3),[1,2,3,4])))
+
+l1 = [-8,2,7,5,-3,5,0,1]
+print(max(l1)+min(l1))
+
+n = 17 / 3
+print(round(n,4)) #n을 4자리 수까지 반올림
