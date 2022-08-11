9c46cfd94f95359c7f4a0b31f6cb085f221e9009
h-wi <108285793+h-wi@users.noreply.github.com>
2022-08-11 11:30:18 +0900

    Update 18258.py

--- 큐/18258.py
+++ 큐/18258.py
@@ -1,30 +1,31 @@
-n = int(input())
-l1 = [] #모든 연산에 대해 big-oh = O(1) 
-front = 0
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+l1 = deque([]) 
 for i in range(n):
-    do = input()
-    if do[0:4] == 'push':
-        l1.append(do[len(do)-1])
-    elif do[0:3] == 'pop':
-        try:
-            print(l1[front])
-            front += 1
-            #print(l1.pop(0)) pop의 연산복잡도 맨 마지막은 O(1)이지만 특정 인덱스는 O(n), 제거된 값 이후로 다 요소들을 앞으로 재배치하기 때문에
-        except:
+    do = sys.stdin.readline().split()
+    if do[0] == 'push':
+        l1.append(do[1])
+    elif do[0] == 'pop':
+        if not l1:
             print('-1')
-    elif do[0:4] == 'size':
-        print(len(l1)-front) #len의 연산복잡도 O(1)
-    elif do[0:5] == 'empty':
-        if len(l1)-front == 0: 
+        else:
+            print(l1.popleft())
+    elif do[0] == 'size':
+        print(len(l1)) 
+    elif do[0] == 'empty':
+        if len(l1) == 0: 
             print('1')
         else:
             print('0')
-    elif do[0:5] == 'front':
+    elif do[0] == 'front':
         try:
-            print(l1[front]) #O(1)
+            print(l1[0]) 
         except:
             print('-1')
-    elif do[0:4] == 'back':
+    elif do[0] == 'back':
         try:
-            print(l1[len(l1)-1]) #O(1)
+            print(l1[len(l1)-1])
         except:
+            print('-1')
