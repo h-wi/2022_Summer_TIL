84cf0b924f63d65a92bc1151f688ee12d6fc7140
h-wi <108285793+h-wi@users.noreply.github.com>
2022-08-14 21:50:24 +0900

    Update 10816.py

--- 이진탐색/10816.py
+++ 이진탐색/10816.py
@@ -1,28 +1,30 @@
-def binary_search(arr,target,start,end):
-
-    if start > end:
-        return 0
-    mid = (start + end) // 2
-
-    if arr[mid] == target:
-        return mid
-    elif arr[mid] > target:
-        binary_search(arr,target,start,mid-1)
-    else:
-        binary_search(arr,target,mid+1,end)
-
+def binary_search(arr,target,start,end): #binary search에서 중복 횟수를 찾으면 시간초과
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target:
+            return hash[target]
+        elif arr[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+ 
 n = int(input())
 lst = list(map(int,input().split()))
 m = int(input())
 check = list(map(int,input().split()))
 
+hash = {} #dictionary를 이용해서 얼마나 중복됐는지 미리 저장 
+
+for i in lst:
+    if i in hash:
+        hash[i] += 1
+    else:
+        hash[i] = 1
+
 lst.sort()
 for i in range(m):
-    count = 0
-    while binary_search(lst,check[i],0,len(lst)-1) != 0:
-        count += 1
-        try:
-            lst.remove(check[i])
-        except:
-            break
-    print(count-1, end=' ')
+    result = binary_search(lst,check[i],0,n-1)
+    if result:
+        print(result, end = ' ')
+    else:
+        print(0, end = ' ')
