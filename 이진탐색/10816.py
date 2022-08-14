3217a2a713cf749464b3d7798641b4817bec5938
h-wi <108285793+h-wi@users.noreply.github.com>
2022-08-14 21:00:52 +0900

    Update 10816.py

--- 이진탐색/10816.py
+++ 이진탐색/10816.py
@@ -1,4 +1,15 @@
-import bisect
+def binary_search(arr,target,start,end):
+
+    if start > end:
+        return 0
+    mid = (start + end) // 2
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        binary_search(arr,target,start,mid-1)
+    else:
+        binary_search(arr,target,mid+1,end)
 
 n = int(input())
 lst = list(map(int,input().split()))
@@ -7,5 +18,11 @@ check = list(map(int,input().split()))
 
 lst.sort()
 for i in range(m):
-    count = bisect.bisect_right(lst,check[i])-bisect.bisect_left(lst,check[i])
-    print(count, end=' ')
+    count = 0
+    while binary_search(lst,check[i],0,len(lst)-1) != 0:
+        count += 1
+        try:
+            lst.remove(check[i])
+        except:
+            break
+    print(count-1, end=' ')
