2969732f7a5851e40ed8defbaf81f19bbfb48e9b
h-wi <108285793+h-wi@users.noreply.github.com>
2022-08-24 16:05:41 +0900

    Update dfs_24479.py

--- dfs_24479.py
+++ dfs_24479.py
@@ -1,34 +1,22 @@
 import sys
 sys.setrecursionlimit(10**6)
+n,m,r = map(int,sys.stdin.readline().split())
+adj_lst = [[] for vertex in range(n)]
+visited = [0 for _ in range(n+1)]; count = 1
+for edge in range(m):
+  u,v = map(int,sys.stdin.readline().split())
+  adj_lst[u-1].append(v)
+  adj_lst[v-1].append(u)
+for vertex in range(n):
+  adj_lst[vertex].sort()
 
-global c
-c = 1
-def dfs(adjac_lst,r):
-    global c
-    visited[r-1] = c
-    for i in adjac_lst[r-1]:
-        if not visited[i-1]:
-            c += 1
-            dfs(adjac_lst,i)
-
-n,m,r = map(int,input().split())
-edges = []
-adjac_lst = [[] for vertex_number in range(n)] #adjacent list를 만들기 위해 vertex의 갯수만큼 미리 리스트 생성
-visited = [0 for visit in range(n)]
-
-for i in range(m):
-    u,v = map(int,input().split())
-    edges.append((u,v)) #tuple형태로 edge를 받아준다
-
-edges.sort(key=lambda x: x[1]) #vertex를 오름차순으로 방문하기 위해 정렬
-
-for edge in edges:
-    adjac_lst[edge[0]-1].append(edge[1])
-    adjac_lst[edge[1]-1].append(edge[0]) #undirected edge이므로 번갈아 추가
-
-dfs(adjac_lst,r)
-
-for i in visited:
-    print(i)
-
-
+def dfs(adj_lst,r):
+  global count
+  visited[r] = count
+  count += 1
+  for i in adj_lst[r-1]:
+    if not visited[i]:
+      dfs(adj_lst,i)
+dfs(adj_lst,r)
+for i in range(1,n+1):
+  print(visited[i])
